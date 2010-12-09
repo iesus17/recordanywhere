f43db2fd37aa60d63138666f7459d575a0d1e0f1
shifeng222@gmail.com <shifeng222@gmail.com@d082e9dd-5bed-51b6-96ed-b7f51e0bafd0>
2010-12-09 06:01:52 +0000

    Class definitions in Common project

---
+++ trunk/WebJournal/Common/CSVUtil.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace GBSSOAPTest
+{
+    /// <summary>
+    /// CSVUtil is a helper class handling csv files.
+    /// </summary>
+    public class CSVUtil
+    {
+        private CSVUtil()
+        {
+        }
+        //write a new file, existed file will be overwritten
+        public static void WriteCSV(string filePathName, List<String[]> ls)
+        {
+            WriteCSV(filePathName, false, ls);
+        }
+        //write a file, existed file will be overwritten if append = false
+        public static void WriteCSV(string filePathName, bool append, List<String[]> ls)
+        {
+            StreamWriter fileWriter = new StreamWriter(filePathName, append, Encoding.Default);
+            foreach (String[] strArr in ls)
+            {
+                fileWriter.WriteLine(String.Join(",", strArr));
+            }
+            fileWriter.Flush();
+            fileWriter.Close();
+
+        }
+        public static List<String[]> ReadCSV(string filePathName)
+        {
+            List<String[]> ls = new List<String[]>();
+            StreamReader fileReader = new StreamReader(filePathName);
+            string strLine = "";
+            while (strLine != null)
+            {
+                strLine = fileReader.ReadLine();
+                if (strLine != null && strLine.Length > 0)
+                {
+                    ls.Add(strLine.Split(','));
+                }
+            }
+            fileReader.Close();
+            return ls;
+        }
+
+    }
+}
---
+++ trunk/WebJournal/Common/HtmlTag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebJournal.Common
+{
+    public enum HtmlTag
+    {
+        Button,
+        TextBox,
+        Window
+    }
+}
